3c74881429f2789159286297975886220f299d7b
daften <dieterblomme@gmail.com>
2015-06-08 00:24:13 +0200

    Rubocop corrections

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,14 +1,14 @@
-default["aegir3"]["install_method"] = "package"
-default["aegir3"]["install_folder"] = "/var/aegir"
+default['aegir3']['install_method'] = 'package'
+default['aegir3']['install_folder'] = '/var/aegir'
 
 # Attributes needed for both install methods
-default["aegir3"]["frontend"] = "localhost"
-default["aegir3"]["admin_email"] = "webmaster@localhost"
-default["aegir3"]["webserver"] = "apache2"
+default['aegir3']['frontend'] = 'localhost'
+default['aegir3']['admin_email'] = 'webmaster@localhost'
+default['aegir3']['webserver'] = 'apache2'
 
 default['mysql_tuning']['tuning.cnf']['mysqld']['max_allowed_packet'] = '16M'
 
-default["php"]["conf_dir"] = "/etc/php5/apache2"
-default["php"]["directives"] = {
-    "memory_limit" => "256M"
+default['php']['conf_dir'] = '/etc/php5/apache2'
+default['php']['directives'] = {
+  'memory_limit' => '256M'
 }
--- recipes/package.rb
+++ recipes/package.rb
@@ -21,10 +21,10 @@
 
 if debian?
   apt_repository 'aegir-stable' do
-    uri          'http://debian.aegirproject.org'
+    uri 'http://debian.aegirproject.org'
     distribution 'stable'
-    components   ['main']
-    key          'http://debian.aegirproject.org/key.asc'
+    components ['main']
+    key 'http://debian.aegirproject.org/key.asc'
   end
 
   include_recipe 'mysql::server'
@@ -32,8 +32,8 @@ if debian?
   include_recipe 'mysql_tuning::default'
 
   # Restart MySQL to take changed config into account.
-  service "mysql" do
-    supports :restart => true, :reload => true
+  service 'mysql' do
+    supports restart: true, reload: true
     action :restart
   end
 
@@ -46,8 +46,8 @@ if debian?
   include_recipe 'php::ini'
 
   # Restart apache for php.ini changes to take effect
-  service "apache2" do
-    supports :restart => true, :reload => true
+  service 'apache2' do
+    supports restart: true, reload: true
     action :reload
   end
 end
