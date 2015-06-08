250b693fcceb3de0584e3c4302957dc639622834
daften <dieterblomme@gmail.com>
2015-06-08 02:36:21 +0200

    Version bump to 0.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'info@picabit.be'
 license 'Apache 2.0'
 description 'Hosting System for Drupal Sites'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.1'
+version '0.1.2'
 recipe 'aegir3', 'Install aegir with the chosen method (default: package)'
 recipe 'aegir3::package', 'Install aegir from package'
 recipe 'aegir3::script', 'Install aegir with nginx and php-fpm'
