2dbc83733fac29c7d392ca37ec7a5a6bbefbbcd0
daften <dieterblomme@gmail.com>
2015-06-08 02:07:03 +0200

    Support for php location for php.ini changes

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,7 +8,11 @@ default['aegir3']['webserver'] = 'apache2'
 
 default['mysql_tuning']['tuning.cnf']['mysqld']['max_allowed_packet'] = '16M'
 
-default['php']['conf_dir'] = '/etc/php5/apache2'
+if node['aegir3']['webserver'] == 'nginx'
+  default['php']['conf_dir'] = '/etc/php5/fpm/php.ini'
+elsif node['aegir3']['webserver'] == 'apache2'
+  default['php']['conf_dir'] = '/etc/php5/apache2'
+end
 default['php']['directives'] = {
   'memory_limit' => '256M'
 }
