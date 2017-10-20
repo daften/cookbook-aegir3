8fb8ff62b14a147a2e4819860cb5d58ca4b9d276
Dieter Blomme <daften@Dieters-MacBook-Pro.local>
2017-10-20 23:25:17 +0200

    Fixed set chef version in metadata

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,7 @@ license "Apache-2.0"
 description "Hosting System for Drupal Sites"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 version "0.1.4"
+chef_version '>= 12.5' if respond_to?(:chef_version)
 recipe "aegir3", "Install aegir with the chosen method (default: package)"
 recipe "aegir3::package", "Install aegir from package"
 recipe "aegir3::script", "Install aegir with nginx and php-fpm"
